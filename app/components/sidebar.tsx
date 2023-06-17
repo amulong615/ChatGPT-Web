b9c7a90a15eab327a6c76e48143ee96c34dfa42a
A Mulong <124489360+amulong615@users.noreply.github.com>
2023-06-17 15:04:06 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,10 +120,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+        Copyright © 2018-2023 阿木龙. OpenAI 提供技术支持.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
